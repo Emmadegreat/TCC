807085d8c96ede4805a24c6442af489905368464
Emma F. Mkpurunchi <mkpurunchiemma@gmail.com>
2022-05-17 23:20:47 +0100

    committed

--- js/index.js
+++ js/index.js
@@ -32,9 +32,21 @@ $(document).ready(function(){
     $("h1").css("fontSize","1.5rem");
     
     $("#core_value_img_1").mouseleave(function () {
-        $("#body_1").css("backgroundColor","#1d3044").html("Thank you for your patronage.").css("height","20vh").css("color","#fff");
+        $("#body_1").css("backgroundColor","#1d3044").html("Thank you for your patronage.").css("height","20vh").css("fontSize","1.5rem").css("color","#fff");
           
     });
+
+    $("#core_value_img_2").mouseleave(function () {
+        $("#body_2").css("backgroundColor","#1d3044").html("Competency and quality service delivery.").css("height","20vh").css("fontSize","1.5rem").css("color","#fff");
+          
+    });
+
+    $("#core_value_img_3").mouseleave(function () {
+        $("#body_3").css("backgroundColor","#1d3044").html("Your utmost satisfaction is our concern.").css("height","20vh").css("fontSize","1.5rem").css("color","#fff");
+          
+    });
+  
+  
   
     
     
--- main.js
+++ main.js
@@ -2,13 +2,12 @@ class MyHeader extends HTMLElement{
     connectedCallback(){
         this.innerHTML= `
             <header>
-            
             <link rel="stylesheet" href="/css/index.css">
         <link rel="stylesheet" href="/css/bootstrap.css">
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
         <link rel="icon" href="/images/logo.PNG">
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous"> 
-      
+        
             <nav class="navbar fixed-top navbar-expand-sm navbar-dark bg-blue"> <!--navbar-dark bg-dark-->
             <div class="container-fluid">
               <a class="navbar-brand" href="javascript:void(0)">Logo</a>
