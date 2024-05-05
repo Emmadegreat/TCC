8bd074b64f169e4e1c4a552d3c130bcf7473d621
Emma F. Mkpurunchi <mkpurunchiemma@gmail.com>
2024-05-05 15:44:36 +0100

    add

--- js/index.js
+++ js/index.js
@@ -1,4 +1,4 @@
-$(document).ready(function(){
+/*$(document).ready(function(){
     /*$("button").click(function(){
         $("h1").hide();
     });
@@ -8,16 +8,16 @@ $(document).ready(function(){
     });
     $("#p1").hover(function () {
         $(this).html('welcome');
-    }, 
+    },
     function () {
         $(this).html('bye bye');
     });
 
     $("p").css("fontSize","2rem");
-    
+
     */
 
-    
+
 
     $("#fixed_carousel").on({
         mouseenter: function(){
@@ -30,25 +30,25 @@ $(document).ready(function(){
     });
 
     $("h1").css("fontSize","1.5rem");
-    
+
     $("#core_value_img_1").mouseleave(function () {
         $("#body_1").css("backgroundColor","#1d3044").html("Thank you for your patronage.").css("height","20vh").css("fontSize","1.5rem").css("color","#fff");
-          
+
     });
 
     $("#core_value_img_2").mouseleave(function () {
         $("#body_2").css("backgroundColor","#1d3044").html("Competency and quality service delivery.").css("height","20vh").css("fontSize","1.5rem").css("color","#fff");
-          
+
     });
 
     $("#core_value_img_3").mouseleave(function () {
         $("#body_3").css("backgroundColor","#1d3044").html("Your utmost satisfaction is our concern.").css("height","20vh").css("fontSize","1.5rem").css("color","#fff");
-          
+
     });
-  
-  
-  
-    
-    
-});
+
+
+
+
+
+});*/
 /*-----------------jquery file---------------*/
